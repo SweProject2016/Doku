0e153fb0b289602bdea6361ed01c5abbc98551e0
Nilsola <new_nils@hotmail.de>
2016-03-11 15:31:16 +0100

    apackage AClass

---
+++ swe_backend_test/src/apackage/AClass.java
@@ -0,0 +1,20 @@
+/*  
+ * ----------------------------------------------------------------------------
+ *     (c) by data experts gmbh
+ *            Postfach 1130
+ *            Woldegker Str. 12
+ *            17001 Neubrandenburg
+ * ----------------------------------------------------------------------------
+ *     Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+ *     dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+ *     Herausgebers weder als ganzes noch in Teilen dupliziert, reproduziert
+ *     oder manipuliert werden.
+ * ----------------------------------------------------------------------------
+ *     
+ * ----------------------------------------------------------------------------
+ */
+package apackage;
+
+public class AClass {
+
+}
